0a253727b6424b272391f121bbe6bc371f0ef8c0
Ein anderer User <einandereruser@archimedes.localdomain>
2011-04-25 20:47:40 +0200

    forgot src/pic.c and inc/pic.h

---
+++ inc/pic.h
@@ -0,0 +1,22 @@
+#ifndef PIC_H
+#define PIC_H
+
+#include "stdint.h"
+
+#define PIC_MCOMMAND	0x20
+#define PIC_MDATA		0x21
+#define PIC_MIMR		0x21
+#define PIC_SCOMMAND	0xA0
+#define PIC_SDATA		0xA1
+#define PIC_SIMR		0xA1
+
+void pic_init(void);
+
+void remap_int(int num);
+void pic_mask_irqs(uint16_t mask);
+void pic_send_eoi(int irq);
+
+void outb(uint16_t port, uint8_t data);
+
+
+#endif
